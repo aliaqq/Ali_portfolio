516d0ce3edf019fe2548e6da2741ff574ff234d0
aliaqq <162880882+aliaqq@users.noreply.github.com>
2024-03-24 10:57:36 +0000

    add reqirements

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
-from flask import Flask,render_template
+from flask import Flask, render_template,request
+from database import load_projects_from_db, load_project_from_db
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-  return render_template("home.html")
-  
 
-if __name__ == '__main__':
-  app.run(host='0.0.0.0', debug=True)
+@app.route("/")
+def hello_world():
+    projects = load_projects_from_db()
+    return render_template('home.html', projects=projects)
+
+
+@app.route("/project/<id>")
+def project(id):
+    project = load_project_from_db(id)
+    return render_template('project.html', project=project)
+
+
+@app.route("/massage")
+def massage():
+    return render_template('contactus.html')
+
+@app.route("/massage_submitted",methods=['POST'])
+def massage_submitted():
+    data = request.form
+    return render_template('massage_submitted.html',data=data)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -1,9 +1,15 @@
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine,text
+import os 
+engine = create_engine(os.environ['DB_CONNECTION_STRING' ])
 
-engine = create_engine(
-    "mysql+pymysql://admin:1234admin@database-1.cjwkwck8gbcr.eu-north-1.rds.amazonaws.com/projects?charset=utf8mb4"
-)
-with engine.connect() as conn:
-  result = conn.execute(text("select * from trial"))
-  print(result.all())
-#interaction between mysql and python 
+def load_projects_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from Myprojects"))
+    projects = []
+    for row in result.all():
+      projects.append(dict(row._mapping))
+  return projects
+def load_project_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from Myprojects where id = :val"),{"val":id})
+  rows =dict(rows)[0]._mapping
